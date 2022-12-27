b0bf0d5e73fd47794a263696a481cf03ac48611e
Guilherme <gui_g88@hotmail.com>
2022-12-27 14:24:44 -0300

    added an id to the user item

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,37 @@
 import { instance } from '@api/http-common';
 import { useEffect, useState } from 'react';
+import { v4 } from 'uuid';
+
+type User = { name: string; email: string; phone: string };
+
+type Data = { users: User[] };
 
 export default function App() {
-  const [users, usersSet] = useState([]);
+  const [users, usersSet] = useState([] as User[]);
 
   useEffect(() => {
     instance
-      .get('/users')
+      .get<Data>('/users')
       .then((response) => usersSet(response.data.users))
       .catch((error) => error)
       .finally(() => {});
     return () => {};
   }, []);
 
-  return <UsersList users={users} />;
+  return <UserList users={users} />;
 }
 
-function UsersList({ users }: { users: { id: number; name: string }[] }) {
+interface IUserList extends Data {}
+
+function UserList({ users }: IUserList) {
+  const newUsers = users.map((user) => ({ ...user, id: v4() }));
+
   return (
     <div className='h-screen bg-indigo-500 text-indigo-100'>
       <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
+        {newUsers.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
       </ul>
     </div>
   );
